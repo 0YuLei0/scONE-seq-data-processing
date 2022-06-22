2c4b31b72d01daf60f846e50ca8dd59441574288
0YuLei0 <91858841+0YuLei0@users.noreply.github.com>
2022-06-22 11:29:24 +0800

    Update Separation_bash.sh

--- RawDataProcessing/Separation_bash.sh
+++ RawDataProcessing/Separation_bash.sh
@@ -6,8 +6,8 @@ fastp --thread=24 --disable_adapter_trimming --trim_tail1=1 --trim_poly_g --trim
 ## 2. For MGI reads, to avoid later problem in DNA bwa mapping, we should rename the fastq head with Illumina format
 zcat temp_fp_r1.fastq.gz |awk '{if(NR%4==1) gsub(/\/1/," 1:N:0"); print $0; }' | pigz > fp_r1.fastq.gz
 zcat temp_fp_r2.fastq.gz |awk '{if(NR%4==1) gsub(/\/2/," 2:N:0"); print $0; }' | pigz > fp_r2.fastq.gz
-## 3. Use cutadapt to demultiplex DNA/RNA reads
-cutadapt --quiet -e 0.4 -g file:/home/lyuah/software/DR_sep/version2.2_mgi/dr_barcode.fasta -o trimmed-{name}.R2.fastq.gz -p trimmed-{name}.R1.fastq.gz fp_r2.fastq.gz fp_r1.fastq.gz
+## 3. Use cutadapt to demultiplex DNA/RNA reads, PLEASE REMEMBER TO CHANGE THE PATH TO scONEseq_barcode.fasta
+cutadapt --quiet -e 0.4 -g file:$PATH/scONEseq_barcode.fasta -o trimmed-{name}.R2.fastq.gz -p trimmed-{name}.R1.fastq.gz fp_r2.fastq.gz fp_r1.fastq.gz
 ## 4. Extract RNA umi from fastq files with fastp
 fastp --thread=24 --length_required=50 --disable_adapter_trimming --disable_quality_filtering -U --umi_loc=read1 --umi_len=8 --umi_prefix=umi -i trimmed-5RNA.R2.fastq.gz -o rna_fp_r2.fastq.gz -I trimmed-5RNA.R1.fastq.gz -O rna_fp_r1.fastq.gz --json="rna".json --html="rna".html --report_title="rna"
 ## 5. Extract DNA umi and remvoe Tn5 mosaic sequeces
