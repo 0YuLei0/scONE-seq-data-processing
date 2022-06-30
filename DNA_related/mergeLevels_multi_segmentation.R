81feaf6b437c57fde8965ddc196d0e6dc7291cf6
0YuLei0 <91858841+0YuLei0@users.noreply.github.com>
2022-06-30 16:49:54 +0800

    Update mergeLevels_multi_segmentation.R

--- DNA_related/mergeLevels_multi_segmentation.R
+++ DNA_related/mergeLevels_multi_segmentation.R
@@ -1,5 +1,6 @@
 ## R function to perform segmentation with"copynumber" and merge segmentation with "aCGH"
 cytoband_hg38 <- "http://hgdownload.cse.ucsc.edu/goldenpath/hg38/database/cytoBand.txt.gz"
+cb <- data.table::fread(cytoband_hg38, header = FALSE, data.table = FALSE)
 cb <- cb[cb$V1 %in% c(paste0("chr", 1:22), "chrX", "chrY"), ]
 rownames(cb) <- seq(1:nrow(cb))
 cb$V1 <- factor(cb$V1)
